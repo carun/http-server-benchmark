6ef6a6bb6d1dd9e0692bf3b61c613232bac74442
Arun Chandrasekaran <arun.chandrasekaran@necam.com>
2020-02-20 08:38:42 -0800

    Use manual memory management for improved performance.

--- src/app.d
+++ src/app.d
@@ -5,22 +5,33 @@ import std.experimental.logger: FileLogger, MultiLogger;
 __gshared MultiLogger _log;
 __gshared FileLogger onScreen;
 
+import core.time;
+import core.thread;
+
+alias sleep = compose!(Thread.sleep, dur!("seconds"));
+
 void main()
 {
-    auto settings = new HTTPServerSettings;
-    settings.port = 3000;
-    settings.bindAddresses = ["::1", "127.0.0.1"];
-    listenHTTP(settings, &hello);
-
     _log = new MultiLogger();
-    onScreen = new FileLogger(stdout);
-    _log.insertLogger("stdout", onScreen);
-
+    _log.insertLogger("stdout", new FileLogger(stdout));
+    writefln("Master %d is running", getpid());
+    setupWorkerThreads(logicalProcessorCount);
+    runWorkerTaskDist(&runServer);
     runApplication();
 }
 
-void hello(HTTPServerRequest req, HTTPServerResponse res)
+void runServer()
 {
-    _log.info("Hello, world!");
+    auto settings = new HTTPServerSettings;
+    settings.options |= HTTPServerOption.reusePort;
+    settings.port = 8080;
+    settings.bindAddresses = ["127.0.0.1"];
+    listenHTTP(settings, &handleRequest);
+}
+
+void handleRequest(HTTPServerRequest req, HTTPServerResponse res)
+{
+    // _log.info("Hello, world!");
+    // sleep(1);
     res.writeBody("Hello World");
 }
