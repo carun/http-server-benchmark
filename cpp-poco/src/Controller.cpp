ede4a855ad6cc35549adeef38317d2e689516942
Arun Chandrasekaran <arun.chandrasekaran@necam.com>
2020-11-15 19:33:58 -0800

    feat: HTTPS and code coverage example.

--- cpp-poco/src/Controller.cpp
+++ cpp-poco/src/Controller.cpp
@@ -3,6 +3,13 @@
 /// @copyright  NEC Corporation of America
 ///
 
+#include <Poco/Net/SSLManager.h>
+#include <Poco/SharedPtr.h>
+#include <Poco/Net/KeyConsoleHandler.h>
+#include <Poco/Net/ConsoleCertificateHandler.h>
+#include <Poco/Net/SecureServerSocket.h>
+#include <Poco/Net/HTTPServerRequestImpl.h>
+
 #include "Controller.hpp"
 #include "RestHandler.hpp"
 
@@ -30,27 +37,31 @@ Controller::~Controller()
 void Controller::initialize()
 {
     using namespace Poco::Net;
+    using namespace Poco;
     auto* factory = new ESRequestHandlerFactory;
     auto* params = new HTTPServerParams;
     int backlog = 128;
-    char* bk = std::getenv("AIMES_TCP_BACKLOG");
-    if (bk != NULL)
-        backlog = std::stoi(bk, NULL, 0);
-    // This is a memory leak.
-    // It is one time memory allocation that is never deallocated.
-    // Other design is to have this variable as class member and that will complicate things further.
-    auto* serverSock = new ServerSocket(8080, backlog);
-
     params->setMaxThreads(8);
 
     try
     {
+        SharedPtr<PrivateKeyPassphraseHandler> pConsoleHandler = new KeyConsoleHandler(true);
+        SharedPtr<InvalidCertificateHandler> pInvalidCertHandler = new ConsoleCertificateHandler(true);
+        Context::Ptr pContext = new Context(Context::SERVER_USE, "privatekey.pem", "privatekey.pem", "rootcert.pem", Context::VERIFY_NONE, 9, false, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+        pContext->disableProtocols(Poco::Net::Context::PROTO_SSLV2 | Poco::Net::Context::PROTO_SSLV3);
+        SSLManager::instance().initializeServer(pConsoleHandler, pInvalidCertHandler, pContext);
+
+        // This is a memory leak.
+        // It is one time memory allocation that is never deallocated.
+        // Other design is to have this variable as class member and that will complicate things further.
+        auto* serverSock = new SecureServerSocket(5050, backlog, pContext);
         _httpServer = std::make_unique<HTTPServer>(factory, *serverSock, params);
         _httpServer->start();
 
     }
     catch (Poco::Net::NetException& e)
     {
+        std::cout << "HTTPS server exception: " <<  e.displayText() << std::endl;
         throw std::runtime_error(e.displayText());
     }
 }
